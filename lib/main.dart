8e9d38b174ec4c3639bc325d64844ea5b392a06d
jayasaleh <salehjaya97@gmail.com>
2025-07-24 09:20:11 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:to_do/firebase_options.dart';
+import 'package:to_do/routes/routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      routerConfig: ref.watch(goRouterProvider),
+      title: 'To Do App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 43, 1, 149),
+        ),
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: const Color.fromARGB(255, 1, 19, 132),
+          centerTitle: true,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData().copyWith(
+          backgroundColor: Colors.blue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:go_router/go_router.dart';
+import 'package:to_do/features/authentication/presentation/screens/login_screen.dart';
+import 'package:to_do/features/authentication/presentation/screens/register_screen.dart';
+import 'package:to_do/features/task_management/presentation/screens/main_screen.dart';
+import 'package:to_do/routes/go_router_refresh_stream.dart';
+
+part 'routes.g.dart';
+
+enum AppRoute { main, login, register }
+
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
+@riverpod
+GoRouter goRouter(GoRouterRef ref) {
+  final firebaseAuth = ref.watch(firebaseAuthProvider);
+  return GoRouter(
+    initialLocation: '/main',
+    debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final isLoggedIn = firebaseAuth.currentUser != null;
+      if (isLoggedIn &&
+          (state.uri.toString() == '/login' ||
+              state.uri.toString() == '/register')) {
+        return '/main';
+      } else if (!isLoggedIn && state.uri.toString().startsWith('/main')) {
+        return '/login';
+      }
+      return null;
+    },
+    refreshListenable: GoRouterRefreshStream(firebaseAuth.authStateChanges()),
+    routes: [
+      GoRoute(
+        path: '/main',
+        name: AppRoute.main.name,
+        builder: (context, state) => const MainScreen(),
+      ),
+      GoRoute(
+        path: '/login',
+        name: AppRoute.login.name,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        name: AppRoute.register.name,
+        builder: (context, state) => const RegisterScreen(),
+      ),
+    ],
+  );
+}
